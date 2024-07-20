66d787ff3acd931adb07aa9bba9d85699d89e142
Rony <kordrony@gmail.com>
2024-07-20 13:59:03 -0400

    original format

---
+++ client/src/assets/Close.js
@@ -0,0 +1,9 @@
+const Close = () => {
+    return (
+        <svg width="35" height="35" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M7.05033 16.9497L16.9498 7.05025M7.05033 7.05025L16.9498 16.9497" stroke="#714A74" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    )
+}
+
+export default Close;
