addc65e61b3f7302bd1a1a216dfa15bcad22b9b2
Rony <kordrony@gmail.com>
2024-07-20 13:59:23 -0400

    original format

--- src/assets/Hamburger.js
+++
@@ -1,9 +0,0 @@
-const Hamburger = () => {
-    return (
-        <svg width="35" height="35" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M3 6H21M3 12H21M3 18H21" stroke="#714A74" strokeLinecap="round" strokeLinejoin="round" />
-        </svg>
-    )
-}
-
-export default Hamburger;
